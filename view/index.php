910407b7bffd59bbb4750db2919f074a93eae3af
DarDevilX <SamLirsuy5542>
2021-11-03 12:53:35 +0700

    Push keberapa ini

--- view/index.php
+++ view/index.php
@@ -13,7 +13,8 @@
     <script src="../public/jquery/jquery-3.3.1.min.js"></script>
     <link href="../public/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="../public/assets/css/plugins.css" rel="stylesheet" type="text/css" />
-    <link rel='stylesheet' href='sweetalert2.css'>
+    <link rel='stylesheet' href='../public/sweetalert/sweetalert2.css'>
+    <script src="../public/js/add.js"></script>
     <!-- END GLOBAL MANDATORY STYLES -->
     
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
@@ -41,16 +42,19 @@
                           <thead>
                               <tr>
                                   <th class="" hidden>Id</th>
-                                  <th class="checkbox-column">
+                                  <th class="checkbox-column text-center">
                                       <div class="custom-control custom-checkbox checkbox-primary">
                                         <input type="checkbox" class="custom-control-input todochkbox" id="todoAll">
                                         <label class="custom-control-label" for="todoAll"></label>
                                       </div>
                                   </th>
                                   <th class="">Nama</th>
-                                  <th class="">Email</th>
                                   <th class="">Alamat</th>
+                                  <th class="">Email</th>
                                   <th class="">Jurusan</th>
+                                  <th class="">Kelas</th>
+                                  <th class="">Eskul</th>
+                                  <th class="">Organisasi</th>
                                   <th class="text-center">Aksi</th>
                               </tr>
                           </thead>
@@ -58,7 +62,7 @@
                               <?php include '../public/php/script.php';  if($result != null){while($data = mysqli_fetch_assoc($result)): ?>
                               <tr>
                                   <td hidden><?=$data['id']?></td>
-                                  <td class="checkbox-column">
+                                  <td class="checkbox-column text-center">
                                       <div class="custom-control custom-checkbox checkbox-primary">
                                         <input type="checkbox" class="custom-control-input todochkbox" value="<?= $data['id']?>" id="todo-5">
                                         <label class="custom-control-label" for="todo-5"></label>
@@ -70,6 +74,9 @@
                                   <td><?= $data['alamat']?></td>
                                   <td><?= $data['email']?></td>
                                   <td><?= $data['jurusan']?></td>
+                                  <td><?= $data['kelas']?></td>
+                                  <td><?= $data['eskul']?></td>
+                                  <td><?= $data['organisasi']?></td>
                                   <td class="text-center">
                                     <a href="javascript:void(0);" data-toggle="tooltip" data-placement="top" title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 text-success"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
                                     <a href="javascript:void(0);" data-toggle="tooltip" value="<?=$data['id']?>" data-placement="top" title="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 text-danger"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>
@@ -85,7 +92,7 @@
                           </tbody>
                       </table>
                       <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">Add New Data</button>
+                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="exampleModal" >Add New Data</button>
                     <button class="btn btn-danger">Delete Selected Data</button>
                     <!-- Modal -->
                     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -98,28 +105,26 @@
                             </button>
                             </div>
                             <div class="modal-body">
-                        <form method="post" action="script.php">
-                            
-                                <form action="" method="post">
+                                <form action="add.php" method="post">
                                     <div class="form-group">
                                     <label for="nama">Nama</label>
                                     <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Lengkap" >
-                                    </div> <br>
+                                    </div> 
                                     <div class="form-group">
                                     <label for="kelas">Kelas</label>
                                     <select id="kelas" name="kelas" class="form-control">
                                         <option value="10">X</option>
                                         <option value="11">XI</option>
                                     </select> 
-                                    </div><br>
+                                    </div>
                                     <div class="form-group">
                                     <label for="alamat">Alamat</label>
                                     <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Alamat"> 
-                                    </div><br>
+                                    </div>
                                     <div class="form-group">
                                     <label for="email">Email</label>
                                     <input type="email" class="form-control" id="email" name="email" placeholder="Email"> 
-                                    </div><br>
+                                    </div>
                                     <div class="form-group">
                                     <label for="jurusan">jurusan</label>
                                     <select id="jurusan" name="jurusan" class="form-control">
@@ -132,32 +137,32 @@
                                         <option value="EI">Elektronik Industri</option>
                                         <option value="MT">Mekatronika</option>
                                     </select> 
-                                    </div><br>
+                                    </div>
                                     <div class="form-group">
-                                    <label for= "Organisasi">Organisasi</label>
-                                    <select id="Organisasi" name="Organisasi" class="form-control">
-                                        <option value="PMR">Palang Merah Indonesia</option>
-                                        <option value="Paskib">Paskibraka</option>
-                                        <option value="Ambalan">Ambalan</option>
-                                        <option value="BDI">BDI</option>
-                                        <option value="OSIS">OSIS</option>
+                                    <label for= "organisasi">Organisasi</label>
+                                    <select id="organisasi" name="organisasi" class="form-control">
+                                        <option value="pmr">Palang Merah Indonesia</option>
+                                        <option value="paskib">Paskibraka</option>
+                                        <option value="ambalan">Ambalan</option>
+                                        <option value="bdi">BDI</option>
+                                        <option value="osis">OSIS</option>
                                     </select>
-                                    </div> <br>
+                                    </div>
                                     <div class="form-group">
-                                    <label for= "Ekskul">Ekskul</label>
-                                    <select id="Ekskul" name="Ekskul" class="form-control">
-                                        <option value="Basket">Basket</option>
-                                        <option value="Voli">Voli</option>
-                                        <option value="TD">Tarung Derajat</option>
-                                        <option value="MP">Merpati Putih</option>
-                                        <option value="Band">Band</option>
-                                        <option value="Tari">Tari</option>
+                                    <label for= "eskul">Ekskul</label>
+                                    <select id="eskul" name="eskul" class="form-control">
+                                        <option value="basket">Basket</option>
+                                        <option value="voli">Voli</option>
+                                        <option value="td">Tarung Derajat</option>
+                                        <option value="mp">Merpati Putih</option>
+                                        <option value="band">Band</option>
+                                        <option value="tari">Tari</option>
                                     </select>
-                                    </div> <br>
-                                </form>
+                                    </div>
+                                </div>
                             <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button class="btn btn-primary top-right">Kirim</button>
+                            <button class="btn btn-primary top-right" type="submit" name="kirimData" id="kirimData" >Kirim</button>
                             </div>
                         </form>
                         </div>
@@ -178,40 +183,15 @@
     <script src="../public/bootstrap/js/bootstrap.min.js"></script>
     <script src="../public/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
     <script src="../public/assets/js/app.js"></script>
-    </script>
     <script src="../public/assets/js/custom.js"></script>
     <!-- END GLOBAL MANDATORY SCRIPTS -->
-    <?php include '../db.php';
-        if($conn){
-            echo "
-            <script src='sweetalert2.js'></script>	
-            <script>
-            swal({
-            title: 'Connection Success',
-            text : 'Success Connecting To Database',
-            icon : 'success'
-            })
-            </script>
-            ";
-        }else{
-            echo "
-            <script src='sweetalert2.js'></script>	
-            <script>
-            swal({
-            title: 'Connection Failed',
-            text : 'Failed Connecting To Database',
-            icon : 'error'
-            })
-            </script>
-            ";
-            die(mysqli_connect_error($conn));
-        }
-    ?>
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
 
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
-    <script>
-
+    <script  type="text/javascript">
+    $("#todoAll").click(function(){
+        $('input:checkbox').not(this).prop('checked', this.checked);
+    });
     </script>
 </body>
 </html>
