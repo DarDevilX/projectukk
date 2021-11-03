223421b4cbf47c9033cc3c693472fe032ce11123
DarDevilX <SamLirsuy5542>
2021-11-03 15:01:43 +0700

    Assets Up

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ $result = mysqli_query($mysqli, "SELECT * FROM siswa ORDER BY id DESC"); // usin
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
-    <script src="public/jquery/jquery-3.3.1.min.js"></script>
-    <link href="public/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="public/assets/css/plugins.css" rel="stylesheet" type="text/css" />
-    <link rel='stylesheet' href='public/sweetalert/sweetalert2.css'>
+    <!-- <script src="public/jquery/jquery-3.3.1.min.js"></script> -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
+    <link rel='stylesheet' href='assets/sweetalert/sweetalert2.css'>
     <script src="public/js/add.js"></script>
 </head>
 
@@ -77,7 +77,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM siswa ORDER BY id DESC"); // usin
                                   <td><?= $data['organisasi']?></td>
                                   <td class="text-center">
                                     <a href="edit.php?id=<?=$data['id']?>" data-toggle="tooltip" data-placement="top" title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 text-success"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
-                                    <a href="delete.php?id=$data['id']" data-toggle="tooltip" value="<?=$data['id']?>" data-placement="top" title="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 text-danger"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>
+                                    <a href="delete.php?id=<?=$data['id']?>" data-toggle="tooltip" value="<?=$data['id']?>" data-placement="top" title="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 text-danger"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>
                                   </td>
                               </tr>
                                 <?php endwhile;}else{?>
@@ -117,12 +117,9 @@ $result = mysqli_query($mysqli, "SELECT * FROM siswa ORDER BY id DESC"); // usin
     <!-- END MAIN CONTAINER -->
 
     <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
-    <script src="public/assets/js/libs/jquery-3.1.1.min.js"></script>
-    <script src="public/bootstrap/js/popper.min.js"></script>
-    <script src="public/bootstrap/js/bootstrap.min.js"></script>
-    <script src="public/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="public/assets/js/app.js"></script>
-    <script src="public/assets/js/custom.js"></script>
+    <script src="assets/js/libs/jquery-3.1.1.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
     <!-- END GLOBAL MANDATORY SCRIPTS -->
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
 
@@ -132,35 +129,5 @@ $result = mysqli_query($mysqli, "SELECT * FROM siswa ORDER BY id DESC"); // usin
         $('input:checkbox').not(this).prop('checked', this.checked);
     });
     </script>
-</body>
-
-<body>
-<a class="btn btn-primary" href="add.html">Add New Data</a>
-
-	<table width='80%' border=0>
-
-	<tr bgcolor='#CCCCCC'>
-		<td>Nama</td>
-		<td>Alamat</td>
-		<td>Email</td>
-		<td>Jurusan</td>
-		<td>Kelas</td>
-		<td>Organisasi</td>
-		<td>Aksi</td>
-	</tr>
-	<?php 
-	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-	while($res = mysqli_fetch_array($result)) { 		
-		echo "<tr>";
-		echo "<td>".$res['nama']."</td>";
-		echo "<td>".$res['alamat']."</td>";
-		echo "<td>".$res['email']."</td>";	
-		echo "<td>".$res['jurusan']."</td>";
-		echo "<td>".$res['kelas']."</td>";
-		echo "<td>".$res['organisasi']."</td>";
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
-	}
-	?>
-	</table>
 </body>
 </html>
