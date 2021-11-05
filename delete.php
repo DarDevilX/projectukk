60b803e5d713f3c25e7269abb92030ec2065e7d1
DarDevilX <SamLirsuy5542>
2021-11-05 11:01:53 +0700

    Gatau Keberapa

--- delete.php
+++ delete.php
@@ -7,6 +7,7 @@ $id = $_GET['id'];
 
 //deleting the row from table
 $result = mysqli_query($mysqli, "DELETE FROM siswa WHERE id=$id");
+ 
 
 //redirecting to the display page (index.php in our case)
 header("Location:index.php");
